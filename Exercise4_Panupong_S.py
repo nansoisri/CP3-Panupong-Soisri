cf9fd478bec1104f467d3b76fafcaf480a619d63
nansoisri <nansoisri@gmail.com>
2025-07-04 01:36:45 +0700

    Exercise4_Panupong_S

---
+++ Exercise4_Panupong_S.py
@@ -0,0 +1,17 @@
+#กำหนดตัวแปร
+
+foundEn=float(input("Foundation English Score: "))
+genBus=float(input("General Business Score: "))
+introCom=float(input("Introduction to Computer Systems Score: "))
+comPro=float(input("Computer Programming Score: "))
+
+print("ระบบลงคะแนนของนักศึกษาชั้นปีที่ 2 เทอม 1")
+
+print("---------YOUR SCORE----------")
+print("รายวิชาดังต่อไปนี้")
+print("Foundation English: ", foundEn)
+print("General Business: ", genBus)
+print("Introduction to Computer Systems: ", introCom)
+print("Computer Programming: ", comPro)
+
+
