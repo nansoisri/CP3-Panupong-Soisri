c44cce9c9d80fc37118f8235b922f8996ea30e8e
nansoisri <nansoisri@gmail.com>
2025-07-04 01:40:31 +0700

    Update Exercise4_Panupong_S.py

--- Exercise4_Panupong_S.py
+++ Exercise4_Panupong_S.py
@@ -6,8 +6,8 @@ introCom=float(input("Introduction to Computer Systems Score: "))
 comPro=float(input("Computer Programming Score: "))
 
 print("ระบบลงคะแนนของนักศึกษาชั้นปีที่ 2 เทอม 1")
-
 print("---------YOUR SCORE----------")
+
 print("รายวิชาดังต่อไปนี้")
 print("Foundation English: ", foundEn)
 print("General Business: ", genBus)
