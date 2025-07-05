245e3491a7047419705ad09ae55a98001a35a93d
nansoisri <nansoisri@gmail.com>
2025-07-05 11:54:06 +0700

    Exercise5

---
+++ Exercise5_1_Panupong_S.py
@@ -0,0 +1,7 @@
+x = int(input("Enter a number1: "))
+y = int(input("Enter a number2: "))
+
+print(str(x) + " + " + str(y) + " = ", x+y)
+print(str(x) + " - " + str(y) + " = ", x-y)
+print(str(x) + " * " + str(y) + " = ", x*y)
+print(str(x) + " / " + str(y) + " = ", int(x/y))
---
+++ Exercise5_2_Panupong_S.py
@@ -0,0 +1,7 @@
+#สูตร v=s/t
+
+s = int(input("distance: "))
+t = int(input("time: "))
+v = int(s/t)
+
+print(v, "km/h")
